2e47345d29efacc3e827761a37746617734f292b
andreas.kaufmann <andreas.kaufmann@valtech.com>
2022-10-11 13:44:27 +0200

    Site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	const testLinks = [
+		'file://test.txt',
+		'mailto:mom@test.com',
+		'tel:0791235476',
+		'http://localhost:8080/test',
+		'sip:brother@test.com',
+		'skype:sister',
+	]
+</script>
+
+<h1>Home</h1>
+
+{#each testLinks as testLink}
+	<a href={testLink}>{testLink}</a>
+{/each}
+
+<style>
+	a {
+		border-radius: .25rem;
+		border: 1px solid darkgrey;
+		display: block;
+		margin: .25rem;
+		padding: .5rem;
+		text-decoration: none;
+		width: 12rem;
+	}
+
+	a:hover {
+		background: lightgrey;
+	}
+</style>
